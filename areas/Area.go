837f3dd813d2c81d7f2b3fb67e60064ea03ed88b
Adam King <adam.king0126@gmail.com>
2023-12-14 15:40:19 -0600

    remove unused function

--- areas/Area.go
+++ areas/Area.go
@@ -1,8 +1,6 @@
 package areas
 
 import (
-	"database/sql"
-	"fmt"
 	"mud/interfaces"
 )
 
@@ -34,58 +32,4 @@ type AreaInfo struct {
 
 func NewArea() interfaces.AreaInterface {
 	return &Area{}
-}
-
-// TODO: No usages of this function.  Remove?
-func LoadAreaFromDB(db *sql.DB, areaUUID string) (*Area, error) {
-	// Query the database for the area data.
-	area_rows, err := db.Query("SELECT uuid, name, description FROM areas where uuid=?", areaUUID)
-	if err != nil {
-		return nil, err
-	}
-
-	// Make sure that the area exists.
-	if !area_rows.Next() {
-		return nil, fmt.Errorf("Area with UUID %d does not exist", areaUUID)
-	}
-
-	// Create a new Area struct.
-	area := &Area{
-		UUID: areaUUID,
-	}
-
-	// Scan the row and populate the Area struct.
-	err = area_rows.Scan(&area.UUID, &area.Name, &area.Description)
-	if err != nil {
-		return nil, err
-	}
-
-	// Close the rows.
-	if err := area_rows.Close(); err != nil {
-		return nil, err
-	}
-
-	var rooms []Room
-	room_rows, err := db.Query("SELECT uuid, area_uuid, name, description, exit_north, exit_south, exit_west, exit_east, exit_up, exit_down FROM rooms where area_uuid=?", areaUUID)
-	if err != nil {
-		return nil, err
-	}
-
-	for room_rows.Next() {
-		var room Room
-		err := room_rows.Scan(&room.UUID, &room.AreaUUID, &room.Name, &room.Description, &room.Exits.North, &room.Exits.South, &room.Exits.West, &room.Exits.East, &room.Exits.Up, &room.Exits.Down)
-		if err != nil {
-			return nil, err
-		}
-
-		rooms = append(rooms, room)
-
-	}
-	area.Rooms = rooms
-
-	if err := room_rows.Close(); err != nil {
-		return nil, err
-	}
-
-	return area, nil
 }
